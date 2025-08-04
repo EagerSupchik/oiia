490b5eea76e53ac5e0dd34adee66709000e18ebd
Supchik <eagersupchik@gmail.com>
2025-08-04 11:47:00 +0300

    icon added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,12 @@
 import { Navigation } from '@/components/Navigation';
+import Head from 'next/head';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <>
+    <Head>
+      <link rel="icon" href="/logo.png" />
+    </Head>
     <header>
       <Navigation />
     </header>
